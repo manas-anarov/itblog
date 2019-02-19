ff82215190a26a76dbede6556e46f12cb4acd478
manas-anarov <microsoft.kg@mail.ru>
2019-02-19 15:50:39 +0600

    319_1

--- 319/django-project/profile_app/urls.py
+++ 319/django-project/profile_app/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import include, url
 
 from profile_app import views
-from rest_framework.authtoken import views as authviews
-from django.urls import path
 
 urlpatterns = [
-    url(r'^profile/$', views.ShowProfile.as_view(), name='restapp-profile'),
+    url(r'^profile/$', views.ShowProfile.as_view()),
 ]
